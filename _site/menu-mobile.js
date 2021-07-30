b6bb4cf1a93d95d7dd3e39eb5a11e2c655d7dda6
unknown <admin@rpc.karelia.ru>
2021-07-30 19:09:17 +0300

    first commit

---
+++ _site/menu-mobile.js
@@ -0,0 +1,19 @@
+var menu = new MmenuLight(
+	document.querySelector('#menu'),
+	'all'
+);
+var navigator = menu.navigation({
+	// selectedClass: 'Selected',
+	// slidingSubmenus: true,
+	// theme: 'dark',
+	// title: 'Menu'
+});
+var drawer = menu.offcanvas({
+	// position: 'left'
+});
+//	Open the menu.
+document.querySelector('a[href="#menu"]')
+	.addEventListener('click', evnt => {
+		evnt.preventDefault();
+		drawer.open();
+	});
---
+++ index.md
@@ -0,0 +1,9 @@
+---
+layout: index
+description: "ГБУЗ 'Республиканский перинатальный центр' МЗСР РК"
+tags: [Jekyll, theme, responsive, blog, template]
+image:
+  vitrina: rpc-vitrina.jpg
+  credit: ГБУЗ РПЦ
+  # creditlink: http://texturelovers.com
+---
